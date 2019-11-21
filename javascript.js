d5e975d5c8e69e2df2fb3712f7648e6635d6fe2a
MaryEtak <marykatewhite@gmail.com>
2019-11-21 10:04:24 -0500

    I don't know what to write in these lil memos. 'I wrote some code.'

--- javascript.js
+++ javascript.js
@@ -1,4 +1,29 @@
-let characters = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
 
+function randomPasswordLength() {
+    return  (Math.floor(Math.random() * 120) + 8);
+}
+
+function generate() {
+
+
+var characters = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+// var password = 
 // i think this will go somewhere who knows!
-Math.floor(Math.random() * 120) + 8;
+var passwordLength = randomPasswordLength();
+
+var password = '';
+
+for(var i = 0; i < passwordLength; i++){
+
+    var charAt = (Math.floor(Math.random() * characters.length))
+    password = password + characters[charAt]
+
+}
+
+return password;
+}
+
+
+var div = document.getElementById('password');
+
+div.innerHTML += generate();
