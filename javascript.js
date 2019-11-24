4b1a6237970acc2f56fa1a3fde1c1e7e66df37c3
MaryEtak <marykatewhite@gmail.com>
2019-11-23 19:58:10 -0500

    added checked boxes and length input.

--- javascript.js
+++ javascript.js
@@ -1,29 +1,68 @@
 
-function randomPasswordLength() {
-    return  (Math.floor(Math.random() * 120) + 8);
+var generateEl = document.getElementById('generateButton')
+generateEl.addEventListener('click', generate)
+
+
+// var copyEl = document.getElementById('copyButton')
+// getComputedStyle.addEventListener('click', )
+
+
+var uppercase = document.getElementById("uppercase")
+var lowercase = document.getElementById("lowercase")
+var numerals = document.getElementById("numbers")
+var special = document.getElementById("special")
+
+
+function buildArray() {
+    var characters = "";
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWZYZ";
+    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    var numerals = "1234567890";
+    var special = "!@#$%^&*()_+";
+    if (uppercase.checked===true) {
+        characters += upperCase
+    }
+    if (lowercase.checked===true) {
+        characters += lowerCase
+    }
+    if (numerals.checked===true) {
+        characters += numerals
+    }
+    if (special.checked===true) {
+        characters += special
+    }
+    return characters;
 }
+
 
 function generate() {
+    var passwordLength = document.getElementById("slider").value
 
+    var password = '';
+    var characters = buildArray();
 
-var characters = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
-// var password = 
-// i think this will go somewhere who knows!
-var passwordLength = randomPasswordLength();
+    for(var i = 0; i < passwordLength; i++){
 
-var password = '';
+        var charAt = (Math.floor(Math.random() * characters.length))
+        password = password + characters[charAt]
 
-for(var i = 0; i < passwordLength; i++){
-
-    var charAt = (Math.floor(Math.random() * characters.length))
-    password = password + characters[charAt]
-
+    }
+    var passwordBar = document.getElementById('password');
+    passwordBar.textContent += password
 }
 
-return password;
+var generateEl = document.getElementById('generateButton')
+generateEl.addEventListener('click', generate)
+
+
+
+document.getElementById("length").innerHTML = "Length: 25"
+
+document.getElementById("slider").oninput = function() {
+    if (document.getElementById("slider").value > 0) {
+        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+    }
+    else{
+        document.getElementById("length").innerHTML = "Length: 1"
+    }
 }
-
-
-var div = document.getElementById('password');
-
-div.innerHTML += generate();
